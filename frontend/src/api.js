b0f3ea7d9fe79ef67bc9a856aa495ac631a59160
Afif Salim Balde <94882355+Afifsb@users.noreply.github.com>
2025-08-09 23:46:28 +0530

    First commit

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,20 +1,21 @@
 import axios from 'axios';
 
+const API_BASE = process.env.REACT_APP_API_URL || '';
 const api = axios.create({
-    baseURL: '/api', 
-    headers: {
-        'Content-Type': 'application/json'
-    }
+  baseURL: `${API_BASE}/api`, 
+  headers: {
+    'Content-Type': 'application/json'
+  }
 });
 
 api.interceptors.request.use(config => {
-    const token = localStorage.getItem('token');
-    if (token) {
-        config.headers['x-auth-token'] = token;
-    }
-    return config;
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers['x-auth-token'] = token;
+  }
+  return config;
 }, error => {
-    return Promise.reject(error);
+  return Promise.reject(error);
 });
 
 export default api;
